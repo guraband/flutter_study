cbcea894a7004d9ab4aea825a806b69dfb2af5cf
NG <guraband@naver.com>
2026-01-02 23:54:10 +0900

    Part2 > Ch02 > 01 구현

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,10 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("할룽"),
+          title: Text("Widget을 상하좌우로 배치하기"),
           backgroundColor: Colors.blue,
           centerTitle: true,
-          titleTextStyle: TextStyle(color: Colors.white, fontSize: 30),
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
           actions: [
             IconButton(
               onPressed: () {
@@ -18,7 +18,7 @@ void main() {
             ),
           ],
         ),
-        body: TestWidget(),
+        body: Body(),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.bug_report),
           onPressed: () {
@@ -28,6 +28,51 @@ void main() {
       ),
     ),
   );
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 80,
+              color: Colors.red,
+              child: Text("Container 1"),
+            ),
+            Container(
+              width: 100,
+              height: 80,
+              color: Colors.green,
+              child: Text("Container 2"),
+            ),
+            Container(
+              width: 100,
+              height: 80,
+              color: Colors.blue,
+              child: Text("Container 3"),
+            ),
+          ],
+        ),
+        Container(
+          width: 300,
+          height: 120,
+          color: Colors.grey,
+          child: Text("Container 4"),
+        ),
+      ],
+    );
+  }
 }
 
 class TestWidget extends StatelessWidget {

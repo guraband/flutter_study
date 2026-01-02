b80cd36e3eae2182b24a18fea82cf9a70f0dc08d
NG <guraband@naver.com>
2026-01-02 23:27:26 +0900

    Part2 > Ch01 > 05 구현

--- lib/main.dart
+++ lib/main.dart
@@ -35,11 +35,43 @@ class TestWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Center(
-        child: Text(
-          "Hello World!",
-          style: TextStyle(color: Colors.black, fontSize: 50),
+    return Center(
+      child: Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.red,
+            width: 5,
+            style: BorderStyle.solid,
+          ),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              offset: Offset(10, 10),
+              blurRadius: 10,
+              spreadRadius: 5,
+              blurStyle: BlurStyle.normal,
+            ),
+            BoxShadow(
+              color: Colors.blue.withAlpha(100),
+              offset: Offset(-10, -10),
+              blurRadius: 10,
+              spreadRadius: 5,
+              blurStyle: BlurStyle.normal,
+            ),
+          ],
+          color: Color(0xFF9AEFCA),
+        ),
+        // color: Colors.red.shade200,
+        // margin: EdgeInsets.all(10),
+        padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+        child: Center(
+          child: Container(
+            color: Colors.yellow,
+            child: Text('Hello Container'),
+          ),
         ),
       ),
     );
